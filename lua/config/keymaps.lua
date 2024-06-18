739dd96843d40ea96c9fb2b636a326368c10e74e
Benjamen Pyle <benbpyle@gmail.com>
2024-06-18 16:30:18 -0500

    Updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,18 +20,23 @@ vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNavigateNext<CR>", {
     silent = true
 })
 
-vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
-vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
+vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", {})
+vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", {})
 
 -- Toggle autocomplete
 vim.g.cmptoggle = true
 
-local cmp = require('cmp')
-cmp.setup {
+local cmp = require("cmp")
+cmp.setup({
     enabled = function()
         return vim.g.cmptoggle
     end
-}
+})
 vim.keymap.set("n", "<leader>ux", "<cmd>lua vim.g.cmptoggle = not vim.g.cmptoggle<CR>", {
     desc = "toggle nvim-cmp"
 }) -- Toggle autocomplete
+
+vim.keymap.set("n", "gL", "<cmd>lua vim.lsp.buf.hover()<CR>", {
+    desc = "Show buffer hover"
+})
+
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,8 +1,8 @@
 return {
-    'hrsh7th/nvim-cmp',
-    dependencies = {'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'hrsh7th/cmp-nvim-lsp',
-                    'rafamadriz/friendly-snippets', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline',
-                    'hrsh7th/cmp-calc', 'hrsh7th/cmp-emoji', 'hrsh7th/cmp-nvim-lsp-signature-help', {
+    "hrsh7th/nvim-cmp",
+    dependencies = {"L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip", "hrsh7th/cmp-nvim-lsp",
+                    "rafamadriz/friendly-snippets", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-cmdline",
+                    "hrsh7th/cmp-calc", "hrsh7th/cmp-emoji", "hrsh7th/cmp-nvim-lsp-signature-help", {
         "Saecki/crates.nvim",
         event = {"BufRead Cargo.toml"},
         opts = {
@@ -13,30 +13,63 @@ return {
             }
         }
     }},
-    event = 'InsertEnter',
+    event = "InsertEnter",
     config = function()
-        local cmp = require('cmp')
-        local lspkind = require('lspkind')
-        local luasnip = require('luasnip')
+        local cmp = require("cmp")
+        local lspkind = require("lspkind")
+        local luasnip = require("luasnip")
         local cmp_select_opts = {
             behavior = cmp.SelectBehavior.Select
         }
 
         cmp.setup({
             -- disable completion in comments
-
             enabled = function()
-                local in_prompt = vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt'
-                if in_prompt then -- this will disable cmp in the Telescope window (taken from the default config)
+                if require("cmp.config.context").in_treesitter_capture("comment") == true or
+                    require("cmp.config.context").in_syntax_group("Comment") then
                     return false
+                else
+                    return true
                 end
-                local context = require("cmp.config.context")
-                return not (context.in_treesitter_capture("comment") == true or context.in_syntax_group("Comment"))
             end,
+            -- enabled = function()
+            --     local in_prompt = vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt'
+            --     if in_prompt then -- this will disable cmp in the Telescope window (taken from the default config)
+            --         return false
+            --     end
+            --     local context = require("cmp.config.context")
+            --     return not (context.in_treesitter_capture("comment") == true or context.in_syntax_group("Comment"))
+            -- end,
+
             sorting = {
+                -- TODO: Would be cool to add stuff like "See variable names before method names" in rust, or something like that.
                 comparators = {cmp.config.compare.offset, cmp.config.compare.exact, cmp.config.compare.score,
-                               cmp.config.compare.recently_used, cmp.config.compare.kind}
+
+                -- copied from cmp-under, but I don't think I need the plugin for this.
+                -- I might add some more of my own.
+                               function(entry1, entry2)
+                    local _, entry1_under = entry1.completion_item.label:find "^_+"
+                    local _, entry2_under = entry2.completion_item.label:find "^_+"
+                    entry1_under = entry1_under or 0
+                    entry2_under = entry2_under or 0
+                    if entry1_under > entry2_under then
+                        return false
+                    elseif entry1_under < entry2_under then
+                        return true
+                    end
+                end, cmp.config.compare.kind, cmp.config.compare.sort_text, cmp.config.compare.length,
+                               cmp.config.compare.order}
             },
+
+            -- sorting = {
+            --   comparators = {
+            --     cmp.config.compare.offset,
+            --     cmp.config.compare.exact,
+            --     cmp.config.compare.score,
+            --     cmp.config.compare.recently_used,
+            --     cmp.config.compare.kind,
+            --   },
+            -- },
             -- snippet = {
             --     expand = function(args)
             --         luasnip.lsp_expand(args.body)
@@ -47,17 +80,17 @@ return {
                 completion = cmp.config.window.bordered({
                     winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None"
                 }),
-                completeopt = 'menu,menuone,noinsert,noselect'
+                completeopt = "menu,menuone,noinsert,noselect"
             },
             mapping = {
                 -- ['<CR>'] = cmp.mapping.confirm({
                 --     select = false,
                 --     behavior = cmp.ConfirmBehavior.Insert
                 -- }),
-                ['<C-e>'] = cmp.mapping.abort(),
-                ['<Up>'] = cmp.mapping.select_prev_item(cmp_select_opts),
-                ['<Down>'] = cmp.mapping.select_next_item(cmp_select_opts),
-                ['<C-y>'] = cmp.mapping.complete {},
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<Up>"] = cmp.mapping.select_prev_item(cmp_select_opts),
+                ["<Down>"] = cmp.mapping.select_next_item(cmp_select_opts),
+                ["<C-y>"] = cmp.mapping.complete({}),
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(),
@@ -69,7 +102,7 @@ return {
                     cmp.abort()
                     fallback()
                 end,
-                ['<Tab>'] = cmp.mapping(function(fallback)
+                ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
                     elseif luasnip.expand_or_jumpable() then
@@ -77,8 +110,8 @@ return {
                     else
                         fallback()
                     end
-                end, {'i', 's'}),
-                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                end, {"i", "s"}),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
                     elseif luasnip.jumpable(-1) then
@@ -86,16 +119,15 @@ return {
                     else
                         fallback()
                     end
-                end, {'i', 's'})
-
+                end, {"i", "s"})
             },
             sources = cmp.config.sources({{
                 name = "luasnip"
             }, {
-                name = 'path'
+                name = "path"
             }, -- file paths
             {
-                name = 'nvim_lsp',
+                name = "nvim_lsp",
                 entry_filter = function(entry, ctx)
                     if entry:get_kind() == 15 then
                         return false
@@ -103,24 +135,23 @@ return {
                     return true
                 end,
                 keyword_length = 3
-
             }, {
-                name = 'nvim_lsp_signature_help'
+                name = "nvim_lsp_signature_help"
             }, -- display function signatures with current parameter emphasized
             {
-                name = 'nvim_lua',
+                name = "nvim_lua",
                 keyword_length = 2
             }, -- complete neovim's Lua runtime API such vim.lsp.*
             {
-                name = 'buffer',
+                name = "buffer",
                 keyword_length = 2
             }, -- source current buffer
             {
-                name = 'vsnip',
+                name = "vsnip",
                 keyword_length = 2
-            }, -- nvim-cmp source for vim-vsnip 
+            }, -- nvim-cmp source for vim-vsnip
             {
-                name = 'calc'
+                name = "calc"
             }}),
             formatting = {
                 format = function(_, item)
@@ -138,23 +169,22 @@ return {
                 },
                 completion = cmp.config.window.bordered()
             }
-
         })
         -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline({'/', '?'}, {
+        cmp.setup.cmdline({"/", "?"}, {
             mapping = cmp.mapping.preset.cmdline(),
             sources = {{
-                name = 'buffer'
+                name = "buffer"
             }}
         })
 
         -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline(':', {
+        cmp.setup.cmdline(":", {
             mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources({{
-                name = 'path'
+                name = "path"
             }}, {{
-                name = 'cmdline'
+                name = "cmdline"
             }})
         })
 
@@ -170,4 +200,3 @@ return {
         -- })
     end
 }
-
